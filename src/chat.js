59408b5b2e6ede07423c8501f248cee08f112c60
Brian Currie <brian.currie@hotmail.com>
2021-08-11 19:00:18 -0700

    Style, Sentement data aggregation and display

--- src/chat.js
+++ src/chat.js
@@ -3,30 +3,116 @@ import { getSentiment } from './getSentiment';
 function init() {
     const submitBtn = document.getElementById('submitBtn');
     const textInput = document.getElementById('textInput');
-    const display = document.getElementById('sentiment');
+    const chat = document.getElementById('chatHistory');
+    const namebar = document.getElementById('nameBarContainer');
 
     submitBtn.addEventListener('click', () => {
         const text = textInput.value;
-        const sentiment = getSentiment(text);
-        display.innerText = `Sentiment score: ${sentiment.score}, Comparative: ${sentiment.comparative}`;
         textInput.value = '';
+        displayData(text);
         displayUserMessage(text);
-        reply(sentiment);
     });
 
     textInput.addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
             const text = textInput.value;
-            const sentiment = getSentiment(text);
-            display.innerText = `Sentiment score: ${sentiment.score}, Comparative: ${sentiment.comparative}`;
             textInput.value = '';
+            displayData(text);
             displayUserMessage(text);
-            reply(sentiment);
+        }
+    });
+
+    namebar.addEventListener('click', (e) => {
+        if (e.target.id === 'minimize') {
+            if (chat.style.display !== 'none') {
+                chat.style.display = 'none';
+                namebar.classList.add('pointer');
+            }
+        } else {
+            if (chat.style.display === 'none') {
+                chat.style.display = 'flex';
+                namebar.classList.remove('pointer');
+            }
         }
     });
 }
 
+function displayData(text) {
+    const display = document.getElementById('sentiment');
+    const sentimentData = getSentiment(text);
+    const table = document.createElement('table');
+
+    if (sentimentData === -1) {
+        console.log('No data to display');
+        return -1;
+    }
+
+    display.innerText = ``;
+
+    const tableHeaders = document.createElement('tr');
+    const propertyHeader = document.createElement('th');
+    const dataHeader = document.createElement('th');
+    propertyHeader.innerText = 'Property';
+    dataHeader.innerText = 'Data';
+
+    table.appendChild(propertyHeader);
+    table.appendChild(dataHeader);
+
+    for (let prop in sentimentData) {
+        const row = document.createElement('tr');
+        const property = document.createElement('td');
+        const data = document.createElement('td');
+        property.innerText = prop;
+
+        if (Array.isArray(sentimentData[prop])) {
+            let filtered = filterArr(sentimentData[prop]);
+
+            console.log(filtered);
+
+            data.innerText = filtered; //insert world list cleaned up
+        } else {
+            data.innerText = sentimentData[prop];
+        }
+
+        row.appendChild(property);
+        row.appendChild(data);
+
+        table.appendChild(row);
+    }
+
+    display.appendChild(table);
+}
+
+function filterArr(arr) {
+    let tempArr = [];
+    let obj = {};
+
+    for (let ele of arr) {
+        if (tempArr.includes(ele)) {
+            obj[ele] += 1;
+        } else {
+            tempArr.push(ele);
+            obj[ele] = 1;
+        }
+    }
+
+    arr = [];
+
+    for (let prop in obj) {
+        if (obj[prop] < 2) {
+            arr.unshift(`${prop}`);
+        } else {
+            arr.unshift(`${prop}(x${obj[prop]})`);
+        }
+    }
+
+    return arr.join(', ');
+}
+
 function displayUserMessage(msg) {
+    if (msg.length <= 0) {
+        return -1;
+    }
     const chat = document.getElementById('chatHistory');
     const msgEle = document.createElement('div');
     msgEle.innerText = msg;
@@ -35,31 +121,6 @@ function displayUserMessage(msg) {
 
     chat.prepend(msgEle);
     msgEle.scrollIntoView();
-}
-
-function reply(sentiment) {
-    const chat = document.getElementById('chatHistory');
-    const msgEle = document.createElement('div');
-    msgEle.innerText = createReply(sentiment);
-    msgEle.classList.add('msg');
-    msgEle.classList.add('botMsg');
-
-    chat.prepend(msgEle);
-    msgEle.scrollIntoView();
-}
-
-function createReply(sentiment) {
-    const score = sentiment.score;
-    let msg;
-    if (score === 0) {
-        msg = 'Neutral';
-    } else if (score > 0) {
-        msg = 'Positive';
-    } else {
-        msg = 'Negative';
-    }
-
-    return msg;
 }
 
 export { init };
--- src/getSentiment.js
+++ src/getSentiment.js
@@ -1,9 +1,34 @@
 import Sentiment from 'sentiment';
+import Compendium from 'compendium-js';
 
 function getSentiment(str) {
+    if (!str) {
+        console.log('String not given');
+        return -1;
+    }
+
+    const input = str.toLowerCase();
     const sentiment = new Sentiment();
-    const result = sentiment.analyze(str);
-    return result;
+    const result = sentiment.analyze(input);
+    const compendProfile = Compendium.analyse(input)[0].profile;
+    const data = parseData(result, compendProfile);
+
+    console.log(result, compendProfile);
+    return data;
+}
+
+function parseData(sentiment, compendium) {
+    const data = {
+        label: compendium.label,
+        sentiment: compendium.sentiment,
+        politeness: compendium.politeness,
+        dirtiness: compendium.dirtiness,
+        negated: compendium.negated,
+        negative_words: sentiment.negative,
+        positive_words: sentiment.positive,
+    };
+
+    return data;
 }
 
 export { getSentiment };
